11cab05948960d20f39fdf18eab5bdd4d9fa29f2
Serafim1979 <korolkov.ivan@mail.ru>
2019-07-04 11:40:23 +0400

    made some minor changes

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main()
 
 	std::cout << "Values: updates = " << updates;
 	std::cout << ", *p_updates = " << *p_updates << std::endl;
+	
+	std::cout << "Address: &updates = " << &updates;
+	std::cout << ", p_updates = " << p_updates << std::endl;
 
 	*p_updates = *p_updates + 1;
 	std::cout << "Now updates = " << updates << std::endl;
