b48a1339a614c836b97a37b20c8c559c4cfff7a6
Serafim1979 <korolkov.ivan@mail.ru>
2019-07-04 11:30:57 +0400

    My commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main()
+{
+	int updates = 6;
+	int * p_updates;
+
+	p_updates = &updates;
+
+	std::cout << "Values: updates = " << updates;
+	std::cout << ", *p_updates = " << *p_updates << std::endl;
+
+	*p_updates = *p_updates + 1;
+	std::cout << "Now updates = " << updates << std::endl;
+
+	system("pause");
+	return 0;
+}
